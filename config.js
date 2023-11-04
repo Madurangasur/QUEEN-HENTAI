72da30605cae2851289c3e6d29db420d03bb0215
Madurangasur <149459235+Madurangasur@users.noreply.github.com>
2023-11-04 19:13:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94740804536', 'madhuwa', true],
+  ['94740804536', 'madhuwa', true], 
   [''] 
 ] //Number of owners
 
